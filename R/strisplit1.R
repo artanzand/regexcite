4a2ecc9964dd03ae571eb08680813d2f01074db0
Artan Zandian <74034506+artanzand@users.noreply.github.com>
2022-01-17 19:51:22 -0800

    create manuals

--- R/strisplit1.R
+++ R/strisplit1.R
@@ -1,5 +1,15 @@
-# splits the words in a string
 
+#' splits the words in a string
+#'
+#' @param x A character vector with one element
+#' @param split what to split on
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' strsplit(x, split = ","
 strsplit1 <- function(x, split) {
     strsplit(x, split = split)[[1]]
 }
